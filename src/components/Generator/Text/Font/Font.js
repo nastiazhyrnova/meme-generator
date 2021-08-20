59030edc4d629ae8840d1934ea42b94929fdc127
Ana <64069789+nas9lug2z@users.noreply.github.com>
2021-08-20 18:32:39 +0200

    adding react-select

--- src/components/Generator/Text/Font/Font.js
+++ src/components/Generator/Text/Font/Font.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import Select from 'react-select';
+import styled from 'styled-components';
+
+const SelectStyled = styled(Select)`
+	display: inline-block;
+	width: 100%;
+`;
 
 const Font = props => {
-	return <div></div>;
+	const options = [
+		{ value: 'Arial', label: 'Arial' },
+		{ value: 'Comic Sans', label: 'Comic Sans' },
+		{ value: 'Pacifico', label: 'Pacifico' },
+	];
+
+	return <SelectStyled options={options} />;
 };
 
 export default Font;
--- src/components/Generator/Text/TextInput.js
+++ src/components/Generator/Text/TextInput.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Input from '../../UI/Input';
